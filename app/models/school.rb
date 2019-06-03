2e548289440e071fb6b73fa7818894cee364526a
RyanZidago <ryanzidago@gmail.com>
2019-06-04 01:26:50 +0200

    fixed association bug between users table and schools table preventing from seeding multiple times due to orphaned records

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,3 @@
 class School < ApplicationRecord
-  has_many :users
+  has_many :users, dependent: :nullify
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+School.destroy_all
 User.destroy_all
 Profile.destroy_all
+
+2.times do
+  School.create!(
+
+  )
+end
 
 10.times do
   User.create!(
     email: Faker::Internet.email,
-    password: "password"
+    password: "password",
+    school_id: School.all.sample.id
   )
   Profile.create!(
     user_id: User.all.sample.id
