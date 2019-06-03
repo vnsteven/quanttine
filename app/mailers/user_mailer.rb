d9663da8e8ac9a2e62616fc7bc9e55631ddb79f8
CamilleRungette <c.rungette@gmail.com>
2019-06-03 15:12:52 +0200

    Implementing Action Mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+	default from: 'no-reply@monsite.fr'
+ 
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+  end
+end
