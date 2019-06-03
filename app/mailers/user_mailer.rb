1142de7e6107ece2a1001ab5e226bd379596e6d2
CamilleRungette <c.rungette@gmail.com>
2019-06-03 16:50:06 +0200

    improved e-mails content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'https://quanttine-staging.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
