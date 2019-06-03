4a5a0deb4f73f045696c66c8c37a222e36bdae80
CamilleRungette <c.rungette@gmail.com>
2019-06-03 15:30:18 +0200

    implementing Sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+	after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def welcome_send
+  	UserMailer.welcome_email(self).deliver_now
+  end
 end
