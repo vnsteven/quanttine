50cd00d326e780c1767285e48f18c367fb515abf
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-04 14:31:13 +0200

    login conditions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require assets/static_page/js/main
 //= require assets/dashboard/js/sb-admin-2
 //= require rails-ujs
 //= require activestorage
