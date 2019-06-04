249f3cb4e6da00bcbe434939c1612b258de5adb4
CamilleRungette <c.rungette@gmail.com>
2019-06-04 14:27:50 +0200

    doing profile page useré

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-School.destroy_all
+# School.destroy_all
 User.destroy_all
 Profile.destroy_all
 
-2.times do
-  School.create!(
+# 2.times do
+#   School.create!(
 
-  )
-end
+#   )
+# end
 
 10.times do
   User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: "password",
     school_id: School.all.sample.id
