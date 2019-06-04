87db23c60a2998b544542f50f6a0caecc10347d3
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-04 18:50:37 +0200

    alert and route modification for user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :dashboard
   resources :user, only: [:show]
   devise_for :users, controllers: { registrations: "registrations" }
   devise_for :admins
+
+  resources :users do
+    resources :dashboard
+  end
 
 end
