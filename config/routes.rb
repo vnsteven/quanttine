43dce9e786d404fdf0b0ad120efe4d30f24cbf6a
RyanZidago <ryanzidago@gmail.com>
2019-06-04 20:31:10 +0200

    added active status to schools

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :admins
 
 ##### FACEBOOK CONNECT #####
-devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, controllers: { registrations: "registrations" }
+#devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, controllers: { registrations: "registrations" }
 
 # devise_scope :users do
 #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_152305) do
+ActiveRecord::Schema.define(version: 2019_06_04_182403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_152305) do
     t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,6 +82,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_152305) do
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["school_id"], name: "index_users_on_school_id"
   end
 
   create_table "views", force: :cascade do |t|
@@ -97,4 +99,5 @@ ActiveRecord::Schema.define(version: 2019_06_04_152305) do
 
   add_foreign_key "admins", "schools"
   add_foreign_key "profiles", "schools"
+  add_foreign_key "users", "schools"
 end
