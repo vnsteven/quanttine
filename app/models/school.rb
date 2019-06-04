bef5a79d87e16a3292eeddac6e03874afb33c6a2
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-06-04 10:58:36 +0200

    created devise for admin

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
+  has_many :admin, dependent: :destroy
   has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_171918) do
+ActiveRecord::Schema.define(version: 2019_06_03_174010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id"
