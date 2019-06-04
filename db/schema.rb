f1e79653235359dc3190b9ffd8401ca995b18a29
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-04 19:53:35 +0200

    logout bug fixed

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_152305) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "school_id"
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
