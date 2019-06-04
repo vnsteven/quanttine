562365647fda86d7d2d8df96b93b9cb2dc5e18d4
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-04 19:52:45 +0200

    log out bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  devise_for :users
 
   resources :users do
     resources :dashboard
@@ -9,12 +10,12 @@ Rails.application.routes.draw do
     resources :dashboard
   end
 
-  resources :user, only: [:show, :index]
+  resources :users, only: [:show, :index]
 
   devise_for :admins
 
 ##### FACEBOOK CONNECT #####
-devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, controllers: { registrations: "registrations" }
+# devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, controllers: { registrations: "registrations" }
 
 # devise_scope :users do
 #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
