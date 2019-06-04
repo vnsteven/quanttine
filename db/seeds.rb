0322425752dadde4d18944e5904fc2b12f40ca00
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-04 15:26:48 +0200

    bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# School.destroy_all
 User.destroy_all
 Profile.destroy_all
+School.destroy_all
+Admin.destroy_all
 
-# 2.times do
-#   School.create!(
 
-#   )
-# end
+2.times do
+  School.create!(
+  )
+end
+
+5.times do
+  Admin.create!(
+    email: Faker::Internet.email,
+    password: "password",
+    school_id: School.all.sample.id
+  )
+end
 
 10.times do
   User.create!(
@@ -22,9 +31,10 @@ Profile.destroy_all
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: "password",
-    school_id: School.all.sample.id
   )
   Profile.create!(
-    user_id: User.all.sample.id
+    user_id: User.all.sample.id,
+    school_id: School.all.sample.id
+
   )
 end
