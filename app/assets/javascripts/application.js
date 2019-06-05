ea82ffa29a4fb9fb647407c5ed45083d6d549725
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-05 16:27:52 +0200

    bug jquery uls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery 
-//= require jquery_ujs
 //= require assets/static_page/js/main
 //= require assets/dashboard/js/sb-admin-2
 //= require rails-ujs
