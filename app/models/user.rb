e2d1b2c887703d3c3befed377d4a0d914704304e
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-06-05 11:06:32 +0200

    cleaned code

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable,
-  :omniauthable, omniauth_providers: %i[facebook]
+  :omniauthable, :omniauth_providers => [:facebook]
 
 
   def welcome_send
@@ -17,8 +17,8 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0, 20]
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
     # user.name = auth.info.name   # assuming the user model has a name
     # user.image = auth.info.image # assuming the user model has an image
     # If you are using confirmable and the provider(s) you use validate emails,
@@ -26,4 +26,13 @@ class User < ApplicationRecord
     # user.skip_confirmation!
     end
   end
+
+def self.new_with_session(params, session)
+  super.tap do |user|
+    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      user.email = data["email"] if user.email.blank?
+    end
+  end
+end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.scoped_views = true
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], token_params: { parse: :json }
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: "https://localhost:3000/users/auth/facebook/callback"
 end
 
