cf32c5dfb1767d53f3c5869ec55a3daa9070d549
CamilleRungette <c.rungette@gmail.com>
2019-06-05 17:53:20 +0200

    student can check one box - backend

--- app/controllers/join_table_profile_preferences_controller.rb
+++ app/controllers/join_table_profile_preferences_controller.rb
@@ -1,6 +1,9 @@
 class JoinTableProfilePreferencesController < ApplicationController
 	def update
-
+		@pref = params[:pref]
+		JoinTableProfilePreference.create!(
+			profile_id: current_user.profile.id, 
+			preference_id: @pref)
 	end
 
 end
