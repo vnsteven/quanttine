bb513a77007333d34dbaf0e5b7a27d49f5f1af5b
RyanZidago <ryanzidago@gmail.com>
2019-06-05 14:16:11 +0200

    implemented enum for Preference.category

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,6 @@
 class Preference < ApplicationRecord
+  enum category: { diet: 0, allergy: 1}
+
   has_many :join_table_profile_preferences, dependent: :destroy
   has_many :profiles, through: :join_table_profile_preferences
 end
