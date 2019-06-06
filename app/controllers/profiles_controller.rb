57dbb7edd336146fc113ce9a5086a7bfe3f9e6df
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-06 13:18:13 +0200

    replace index by show for profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
-	def index
+  def index
+    @profile = Profile.all
+  end
+	def show
 		@user = User.find(current_user.id)
 		@profile = Profile.find_by(user_id: current_user.id)
 	end
@@ -24,7 +27,4 @@ class ProfilesController < ApplicationController
 		)
 
 	end
-
-
-
 end
