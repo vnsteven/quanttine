64200d90cb1f1d4e75365ed6567c27d92f04f812
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-06 19:41:22 +0200

    require jquery ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require assets/static_page/js/main
 //= require assets/dashboard/js/sb-admin-2
 //= require rails-ujs
