24a0ce8edd12a2947ca1cbb55efd207b621417d0
CamilleRungette <c.rungette@gmail.com>
2019-06-06 11:58:48 +0200

    created all the preferencesé

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,9 +45,9 @@ end
 #   )
 # end
 
-2.times do 
+4.times do 
   Preference.create!(
-  name: ["Je ne mange pas de viande", "Je ne mange pas de produits laitiers"].sample,
+  name: ["Je ne mange pas de viande", "Je ne mange pas de produits laitiers", "Je ne mange pas de porc", "Je ne mange pas de produits marins"].sample,
   category: ["diet", "allergy"].sample
 )
 end
