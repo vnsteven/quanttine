4a7d0e735cea50b53de15264e6315b84bf95c281
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-06-07 15:53:08 +0200

    display of menu of the day ok with DB

--- app/controllers/school_meals_controller.rb
+++ app/controllers/school_meals_controller.rb
@@ -6,7 +6,11 @@ class SchoolMealsController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @profile = @user.profile
-    @meals_of_tomorrow = @profile.school.school_meals.find_by(date: Date.tomorrow)
+    @school_meals = @profile.school.school_meals.find_by(date: Date.tomorrow)
+    @starters = @school_meals.servings.where(meal_category: "entrée")
+    @main_dishes = @school_meals.servings.where(meal_category: "plat")
+    @desserts = @school_meals.servings.where(meal_category: "dessert")
+    @sides = @school_meals.servings.where(meal_category: "accompagnement")
 
   end
 end
