6f589c41096cf5776dd21c42222713709c7047b5
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-07 11:15:20 +0200

    form for school meals

--- app/controllers/school_meals_controller.rb
+++ app/controllers/school_meals_controller.rb
@@ -2,4 +2,15 @@ class SchoolMealsController < ApplicationController
   def index
     @school_meals = SchoolMeal.all
   end
+
+  def create
+    @school_meals = SchoolMeal.new(date: params[:date], school_id: current_admin.school_id)
+    if @school_meals.save
+      redirect_to new_admin_school_meal_path(current_admin.id)
+      flash[:success] = "Le repas a bien été créé"
+    else
+      render "new"
+      flash[:error]
+    end
+  end
 end
