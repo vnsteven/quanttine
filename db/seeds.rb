d1d1e4ae8dd0d48e952798d27b8cb980f67ffd95
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-06-07 15:07:35 +0200

    seeds one schoolmeal has 3 categories of servings

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ FoodSupply.destroy_all
 
 2.times do
   School.create!(
-  )
+    )
 end
 
 5.times do
@@ -24,7 +24,7 @@ end
     email: Faker::Internet.email,
     password: "password",
     school_id: School.all.sample.id
-  )
+    )
 end
 
 10.times do
@@ -33,28 +33,28 @@ end
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: "password",
-  )
+    )
 end
 
 4.times do
 
   Preference.create!(
-  name: ["Je ne mange pas de viande", "Je ne mange pas de produits laitiers", "Je ne mange pas de porc", "Je ne mange pas de produits marins"].sample,
-  category: ["diet", "allergy"].sample
-)
+    name: ["Je ne mange pas de viande", "Je ne mange pas de produits laitiers", "Je ne mange pas de porc", "Je ne mange pas de produits marins"].sample,
+    category: ["diet", "allergy"].sample
+    )
 end
 
 20.times do
   JoinTableProfilePreference.create!(
     profile_id: Profile.all.sample.id,
     preference_id: Preference.all.sample.id
-  )
+    )
 end
 
 100.times do
   FoodSupply.create!(
     name: Faker::Food.fruits
-  )
+    )
 end
 
 200.times do
@@ -62,33 +62,61 @@ end
     quantity: rand(1..1000),
     school_id: School.all.sample.id,
     food_supply_id: FoodSupply.all.sample.id
-  )
+    )
 end
 
 i = 1
 50.times do
   School.all.each do |school|
-  SchoolMeal.create!(
-    date: Date.current + i,
-    school_id: school.id
-  )
+    SchoolMeal.create!(
+      date: Date.current + i,
+      school_id: school.id
+      )
   end
   i += 1
 end
 
 
-100.times do
-  Serving.create!(
-    meal_category: rand(1..4),
-    food_supply_id: FoodSupply.all.sample.id,
-    school_meal_id: SchoolMeal.all.sample.id
-  )
+# 100.times do
+#   Serving.create!(
+#     meal_category: rand(1..4),
+#     food_supply_id: FoodSupply.all.sample.id,
+#     school_meal_id: SchoolMeal.all.sample.id
+#   )
+# end
+
+# We create servings (starter/main_course/dessert) for each schoolmeal (aka a daily menu for one school)
+SchoolMeal.all.each do |schoolmeal|
+  3.times do
+    Serving.create!(
+      meal_category: 1,
+      food_supply_id: FoodSupply.all.sample.id,
+      school_meal_id: schoolmeal.id
+      )
+    Serving.create!(
+      meal_category: 2,
+      food_supply_id: FoodSupply.all.sample.id,
+      school_meal_id: schoolmeal.id
+      )
+    Serving.create!(
+      meal_category: 3,
+      food_supply_id: FoodSupply.all.sample.id,
+      school_meal_id: schoolmeal.id
+      )
+    Serving.create!(
+      meal_category: 4,
+      food_supply_id: FoodSupply.all.sample.id,
+      school_meal_id: schoolmeal.id
+      )
+  end
 end
+
+
 
 50.times do
   UserMeal.create!(
     profile_id: Profile.all.sample.id
-  )
+    )
 end
 
 100.times do
@@ -96,12 +124,12 @@ end
     serving_size: rand(1..200),
     serving_id: Serving.all.sample.id,
     user_meal_id: UserMeal.all.sample.id
-  )
+    )
 end
 
 10.times do
   JoinTablePreferenceFood.create!(
     preference_id: Preference.all.sample.id,
     food_supply_id: FoodSupply.all.sample.id
-  )
+    )
 end
