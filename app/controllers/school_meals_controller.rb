a262f33647c1324b7e6414bbb38e0c1db27b99a6
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-08 19:39:47 +0200

    school meal delete

--- app/controllers/school_meals_controller.rb
+++ app/controllers/school_meals_controller.rb
@@ -4,16 +4,23 @@ class SchoolMealsController < ApplicationController
   end
 
   def new
-    @school_meals = SchoolMeal.where(date: Date.tomorrow)
+    @school_meals = SchoolMeal.where(date: Date.tomorrow).order("created_at ASC")
   end
 
   def create
     @food_supply = FoodSupply.find_by(name: params[:food_name])
-    @school_meals = SchoolMeal.create!(date: params[:date], school_id: current_admin.school_id)
+
+    if SchoolMeal.find_by(date: params[:date], school_id: current_admin.school_id) != nil
+      @school_meals = SchoolMeal.create!(date: params[:date], school_id: current_admin.school_id)
+    else 
+      @school_meals = SchoolMeal.find_by(date: params[:date], school_id: current_admin.school_id)
+    end
+
     @servings = Serving.create!(meal_category: params[:meal_category], school_meal_id: @school_meals.id, food_supply_id: @food_supply.id)
-    if @school_meals.save && @servings.save
+    
+    if @servings.save
       redirect_to new_admin_school_meal_path(current_admin.id)
-      flash[:success] = "Le repas a bien été créé"
+      flash[:success] = "Aliment ajouté"
     else
       render "new"
       flash[:error]
