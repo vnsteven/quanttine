f5d938fd7a76138f7166c55352cdfdb4168837e0
RyanZidago <ryanzidago@gmail.com>
2019-06-08 02:43:27 +0200

    Am I really doing twice the same commit ? Where did my work went ? It's amost 3 AM ...

--- app/services/rqr_code_service.rb
+++ app/services/rqr_code_service.rb
@@ -2,7 +2,7 @@ class RqrCodeService
   attr_accessor :qrcode,
 
   def initialize(object)
-    @qr_code = RQRCode::QRCode.new(object, size: 3, level: :h )
+    @qr_code = RQRCode::QRCode.new(object, size: 4, level: :h )
   end
 
 end
