ce7627ef19f0bf6802a1a5ba087cd0bef272c00e
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-08 16:09:18 +0200

    user orders

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admins'
   resources :admins, only: [:show] do
     resources :school_meals
+    resources :user_meals
     resources :quantities, only: [:index]
     resources :statistics, only: [:index]
     resources :profiles, only: [:index]
