8d59f21126ab75eb05d0a7f99721bdcff1ec05bd
RyanZidago <ryanzidago@gmail.com>
2019-06-08 19:08:15 +0200

    added svg qr code for users' profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,14 +1,21 @@
 class Profile < ApplicationRecord
-belongs_to :user
-belongs_to :school, optional: true
-has_many :join_table_profile_preferences, dependent: :destroy
-has_many :preferences, through: :join_table_profile_preferences
-has_many :user_meals, dependent: :destroy
-has_one_attached :avatar
+  belongs_to :user
+  belongs_to :school, optional: true
+  has_many :join_table_profile_preferences, dependent: :destroy
+  has_many :preferences, through: :join_table_profile_preferences
+  has_many :user_meals, dependent: :destroy
+  has_one_attached :avatar
+  after_create :attach_qr_code
 
+  def resized_avatar
+    return self.avatar.variant(resize: '200x200!').processed
+  end
 
-def resized_avatar
-  return self.avatar.variant(resize: '200x200!').processed
-end
+  private
+
+  def attach_qr_code
+    QrCodeService.new(self.id).save_as_svg
+    Profile.update(qr_code: "qr_codes/#{self.id}.svg")
+  end
 
 end
