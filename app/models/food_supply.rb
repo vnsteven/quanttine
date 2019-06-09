9924ff0bf2a961079e59fbfb0b938a80eec57f3a
RyanZidago <ryanzidago@gmail.com>
2019-06-09 16:21:37 +0200

    added validations for FoodSupply model

--- app/models/food_supply.rb
+++ app/models/food_supply.rb
@@ -5,4 +5,12 @@ class FoodSupply < ApplicationRecord
   has_many :school_meals, through: :servings
   has_many :join_table_preference_foods, dependent: :destroy
   has_many :preferences, through: :join_table_preference_foods
+
+  validates :name,
+  presence: true,
+	length: { in: 2..100 },
+	format: { with: /\A[a-zA-Z]+\z/,
+		message: "Uniquement des lettres aucun autre charactère n'est accepté." }
+
+
 end
