4314e85016e4a46e459edbc4cae5fb4b4e7e3c48
RyanZidago <ryanzidago@gmail.com>
2019-06-09 15:59:19 +0200

    added validations for SchoolMeal model

--- app/models/school_meal.rb
+++ app/models/school_meal.rb
@@ -2,4 +2,15 @@ class SchoolMeal < ApplicationRecord
   belongs_to :school
   has_many :servings, dependent: :destroy
   has_many :food_supplies, through: :servings
+
+  validate :check_meal_date
+
+  private
+
+  def check_meal_date
+    if self.date < DateTime.now
+      errors.add(:date, message: "la date de création d'un repas ne peut pas être dans le passé")
+    end
+  end
+
 end
--- app/models/serving.rb
+++ app/models/serving.rb
@@ -5,4 +5,5 @@ class Serving < ApplicationRecord
   belongs_to :food_supply
   has_many :preparing_user_meals, dependent: :destroy
   has_many :user_meals, through: :preparing_user_meals
+
 end
