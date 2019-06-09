f1f1bc02953897d6f675f5dd8cb6cae295e5ecf3
RyanZidago <ryanzidago@gmail.com>
2019-06-09 17:45:50 +0200

    just seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ end
   user = User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
+    email: "#{Faker::Internet.email}",
     password: "password",
   )
   Profile.create!(
