a0c767da94e4dc983dd4e5ea2a2c83e6d11a1d9b
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-10 17:49:04 +0200

    side bar admin dashboard

--- app/controllers/school_meals_controller.rb
+++ app/controllers/school_meals_controller.rb
@@ -3,5 +3,6 @@ class SchoolMealsController < ApplicationController
     @school_meal = SchoolMeal.find_by(date: Date.tomorrow)
     @school_meal.destroy if @school_meal != nil
     redirect_to new_admin_serving_path(current_admin)
+    flash[:success] = "Menu supprimé"
   end
 end
