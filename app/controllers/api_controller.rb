509ee137a4551acf15dce4e87de73454ad4a312c
RyanZidago <ryanzidago@gmail.com>
2019-06-12 01:10:43 +0200

    configuring the queuing method within the api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,8 @@
 class ApiController < ApplicationController
 
   def index
+
+    queueing
   end
 
   def show
@@ -10,14 +12,31 @@ class ApiController < ApplicationController
   end
 
   def create
-    puts params
-    puts
-    puts 'NEXT LINE'
-    puts params['id']
-    puts params['email']
-    puts params['message']
-    puts Profile.find(params[:id]).user.first_name
-    puts 'END OF CREATE ACTION OF API CONTROLLER'
   end
+
+  private
+
+  def queueing
+    if DailyService.find_by_date_and_school_id(
+      params[:current_date],
+      params[:school_id])
+    @daily_service = DailyService.find_by_date_and_school_id(params[:current_date], params[:school_id])
+      @daily_service.queue << params[:profile_id]
+      @daily_service.save
+      puts "queueing the daily_service object that was fetched from the database"
+      puts @daily_service.queue
+    else
+    @daily_service = DailyService.create!(date: params[:current_date], school_id: params[:school_id])
+    @daily_service.queue << params[:profile_id]
+    puts "queueing the daily_service object that was just created by the script"
+    puts @daily_service.queue
+    @daily_service.save
+    end
+  end
+
+def permitted_parameters
+  params.permit('school_id', 'current_date', 'profile_id')
+
+end
 
 end
