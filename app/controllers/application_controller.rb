969b5a85a2ad54076822aaceacaaf48e1bc16584
RyanZidago <ryanzidago@gmail.com>
2019-06-12 01:56:33 +0200

    fixed bug about merger marker leftover in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-<<<<<<< HEAD
-  
-=======
   before_action :login, except: [:index]
 
->>>>>>> development
   protected
 
   def configure_permitted_parameters
