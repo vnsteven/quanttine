e6c211ad6bbaed22e62b14a626ebd781439a5d46
RyanZidago <ryanzidago@gmail.com>
2019-06-11 23:21:34 +0200

    finding profile record based on data sent from the python script post request

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -12,9 +12,12 @@ class ApiController < ApplicationController
   def create
     puts params
     puts
+    puts 'NEXT LINE'
     puts params['id']
     puts params['email']
     puts params['message']
+    puts Profile.find(params[:id])
+    puts 'END OF CREATE ACTION OF API CONTROLLER'
   end
 
 end
