a1f6c3fdf5b49e41ef6de4749ec7c52f4f3486d5
CamilleRungette <c.rungette@gmail.com>
2019-06-12 18:05:47 +0200

    improveed the payment pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ $(function () {
 // AUTO-DISAPPEAR ALERT
 
 window.setTimeout(function() {
-    $(".alert").fadeTo(1500, 0).slideUp(500, function(){
+    $(".alert").fadeTo(2000, 0).slideUp(500, function(){
         $(this).remove(); 
     });
 }, 750);
