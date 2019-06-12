bd9b7a3996b5aae46ce67827def0ec9420051eea
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-06-12 19:01:59 +0200

    refacto controller usermeal

--- app/controllers/user_meals_controller.rb
+++ app/controllers/user_meals_controller.rb
@@ -1,13 +1,11 @@
 class UserMealsController < ApplicationController
-  before_action :set_var
+  before_action :set_variables
   before_action :has_ordered, only: [:destroy, :show]
   before_action :has_not_ordered, only: [:create]
   before_action :menu_tomorrow, only: [:new]
 
 
   def new
-    @user = current_user
-    @profile = User.find(current_user.id).profile
     @school_meals = @profile.school.school_meals.find_by(date: Date.tomorrow)
     @starters = @school_meals.servings.where(meal_category: "entrée")
     @main_courses = @school_meals.servings.where(meal_category: "plat")
@@ -18,23 +16,19 @@ class UserMealsController < ApplicationController
   end
 
   def create
-    @profile = current_user.profile
-    @user = current_user
     @user_meal = UserMeal.create!(profile_id: @profile.id)
     PreparingUserMeal.create!(user_meal_id: @user_meal.id, serving_size: 100, serving_id: params[:user_meal][:starter_choice].to_i)
+    PreparingUserMeal.create!(user_meal_id: @user_meal.id, serving_size: 100, serving_id: params[:user_meal][:main_course_choice].to_i)
     PreparingUserMeal.create!(user_meal_id: @user_meal.id, serving_size: 100, serving_id: params[:user_meal][:main_course_choice].to_i)
     PreparingUserMeal.create!(user_meal_id: @user_meal.id, serving_size: 100, serving_id: params[:user_meal][:dessert_choice].to_i)
     redirect_to user_profile_user_meal_path(@user, @profile, @user_meal.id)
   end
 
   def show
-    @profile = current_user.profile
     @servings = @profile.user_meals.last.servings
   end
 
   def destroy
-    @profile = current_user.profile
-    @user = current_user
     @todays_order = current_user.profile.user_meals.where("created_at >= ?", Time.zone.now.beginning_of_day).last
     todays_order.destroy_all
     redirect_to user_profile_path(@user, @profile)
@@ -47,8 +41,8 @@ class UserMealsController < ApplicationController
   end
 
   def has_ordered
-    @profile = current_user.profile
     @user = current_user
+    @profile = current_user.profile
     unless todays_order.exists?
       flash[:error] = "Vous n'avez pas passé de commande !"
       redirect_to new_user_profile_user_meal_path(@profile, @user)
@@ -56,8 +50,8 @@ class UserMealsController < ApplicationController
   end
 
   def has_not_ordered
-    @profile = current_user.profile
     @user = current_user
+    @profile = current_user.profile
     unless todays_order.empty?
      flash[:error] = "Vous avez déjà passé commande !"
      redirect_to user_profile_user_meal_path(@profile, @user, @user_meal.last.id)
@@ -65,18 +59,17 @@ class UserMealsController < ApplicationController
  end
 
  def menu_tomorrow
-  @profile = current_user.profile
   @user = current_user
+  @profile = current_user.profile
   if current_user.profile.school.school_meals.find_by(date: Date.tomorrow).nil?
     redirect_to user_profile_path(@user, @profile)
     flash[:error] = "Votre école n'a pas encore créé le menu de demain ! Revenez plus tard..."
   end
 end
 
-def set_var
+def set_variables
   @profile = current_user.profile
   @user = current_user
-
 end
 
 
