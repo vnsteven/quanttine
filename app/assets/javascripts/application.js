1595bbe4d9d774debdc6e27188cbc2d3db2f918f
CamilleRungette <c.rungette@gmail.com>
2019-06-12 16:46:23 +0200

    puts the background in assets and created flash for preferences

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ $(function () {
 // AUTO-DISAPPEAR ALERT
 
 window.setTimeout(function() {
-    $(".alert").fadeTo(1000, 0).slideUp(500, function(){
+    $(".alert").fadeTo(1500, 0).slideUp(500, function(){
         $(this).remove(); 
     });
 }, 750);
