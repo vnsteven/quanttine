880b530c3d6c4e8cfa2bee1295bd22762834e70b
RyanZidago <ryanzidago@gmail.com>
2019-06-12 02:04:50 +0200

    I don't really know what I changed ... ????

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,29 @@
+class ApiController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    queueing
+  end
+
+  private
+
+  def queueing
+    if DailyService.find_by_date_and_school_id(
+      params[:current_date],
+      params[:school_id])
+      @daily_service = DailyService.find_by_date_and_school_id(params[:current_date], params[:school_id])
+      @daily_service.queue << params[:profile_id]
+      @daily_service.save
+      puts "queueing the daily_service object n°#{@daily_service.id} that was fetched from the database"
+      puts @daily_service.queue
+    else
+      @daily_service = DailyService.create!(date: params[:current_date], school_id: params[:school_id])
+      @daily_service.queue << params[:profile_id]
+      puts "queueing the daily_service object n°#{@daily_service.id} that was just created by the script"
+      puts @daily_service.queue
+      @daily_service.save
+    end
+  end
+
+end
