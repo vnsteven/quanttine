3c93d4811a456c30b634015c5b87383b31488988
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-13 10:27:42 +0200

    controller tests

---
+++ spec/controllers/school_meals_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe SchoolMeal, type: :controller do
+
+  describe "GET #index" do
+    it 'assigns @school_meals' do
+      school_meal = SchoolMeal.create(date: Date.tomorrow)
+      get :index
+      expect(assigns(:school_meals)).to eq([school_meal])
+    end
+
+    it "renders the index template" do
+    get :index
+    expect(response).to render_template("index")
+  end
+
+end
+
+end
--- spec/controllers/servings_controller_spec.rb
+++ spec/controllers/servings_controller_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.describe ServingsController, type: :controller do
 
   describe "GET #index" do 
-    it "assigns to @servings" do
-      @school_meals = SchoolMeal.create
-      get :index
-    end
+    # it "assigns to @servings" do
+    #   @school_meals = SchoolMeal.create
+    #   get :index
+    # end
   end
 
   describe "GET #new" do 
