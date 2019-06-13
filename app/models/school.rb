0c70f2c60173bfb9c1b13415fb5e92016c101ba7
RyanZidago <ryanzidago@gmail.com>
2019-06-13 15:18:57 +0200

    configuring admin mailer after stripe subscription

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,6 +5,7 @@ class School < ApplicationRecord
   has_many :food_supplies, through: :quantities
   has_many :school_meals, dependent: :destroy
   has_many :daily_services, dependent: :destroy
+  after_update :send_payment_information_email
 
   validates :name,
   presence: true,
@@ -22,5 +23,15 @@ class School < ApplicationRecord
   validates :zipcode,
   presence: true,
   length: { in: 2..50 }
+
+  private
+
+  def send_payment_information_email
+    if self.active?
+      AdminMailer.send_subscription_email(self).deliver_now
+    elsif self.active? == false
+      AdminMailer.send_unsubscription_email(self).deliver_now
+    end
+  end
 
 end
