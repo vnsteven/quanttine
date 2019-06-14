c556b13640ae7fb134f7db50bdc4103621c3311a
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-14 22:58:48 +0200

    clean controllers

--- app/controllers/charge_payments_controller.rb
+++ app/controllers/charge_payments_controller.rb
@@ -1,9 +1,6 @@
 class ChargePaymentsController < ApplicationController
   before_action :authenticate_admin!
 
-  def new
-  end
-
   def create
     @amount = 600
     service = StripeChargeService.new(params, current_admin, @amount)
--- app/controllers/subscription_payments_controller.rb
+++ app/controllers/subscription_payments_controller.rb
@@ -1,9 +1,6 @@
 class SubscriptionPaymentsController < ApplicationController
   before_action :authenticate_admin!
 
-  def new
-  end
-
   def create
     @amount = 50
     service = StripeSubscriptionService.new(params, @amount, current_admin)
