59e869554d40204e23c6bc74d1d041b253b6a440
RyanZidago <ryanzidago@gmail.com>
2019-06-14 03:14:36 +0200

    trying to display daily_service#show view

--- app/controllers/daily_services_controller.rb
+++ app/controllers/daily_services_controller.rb
@@ -9,6 +9,7 @@ class DailyServicesController < ApplicationController
 
   def set_daily_service
     @daily_service = DailyService.find_by(
+      date: Date.today,
       school_id: current_admin.school.id
     )
   end
