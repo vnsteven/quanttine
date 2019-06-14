9967d5eafc797692c2f1c4137b71af868e75257a
RyanZidago <ryanzidago@gmail.com>
2019-06-14 15:29:57 +0200

    uncommenting send_welcome_email after_create for user and admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,7 +4,7 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #after_create :send_welcome_email
+  after_create :send_welcome_email
 
   private
 
