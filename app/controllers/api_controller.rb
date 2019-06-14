4e02fafcdf76414511774090fd167ddd05b5bdb9
RyanZidago <ryanzidago@gmail.com>
2019-06-14 02:51:27 +0200

    small improvement for greate code readability

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,6 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    binding.pry
     QueueingService.new(params).perform
   end
 
