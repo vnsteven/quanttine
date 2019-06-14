888b6950ae2078ec486d77e37a232301bd5bd15b
Van Steven Nguyen <nvguyen@gmail.com>
2019-06-14 15:46:43 +0200

    servicing preparing user meal

---
+++ app/services/create_preparing_user_meal_service.rb
@@ -0,0 +1,20 @@
+class CreatePreparingUserMeal
+
+  def initialize(params, user_meal)
+    @params = params
+    @user_meal = user_meal
+  end
+
+  def perform
+    create_preparing
+  end
+
+  private
+
+  def create_preparing
+    @params.each do |param, value|
+      PreparingUserMeal.create!(user_meal_id: @user_meal.id, serving_size: 100, serving_id: value.to_i)
+    end
+  end
+
+end
